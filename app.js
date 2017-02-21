646f1d9e9cb6b8203c71e3366346dc6c4a0cfa9b
joelpalmer <joel.palmer@demand-energy.com>
2017-02-21 11:11:32 -0600

    iterable and reflect

---
+++ app.js
@@ -0,0 +1,33 @@
+const iterableObject = {
+    [Symbol.iterator]() {
+        const data = ['Go', 'Dodgers'];
+        let index = 0;
+        return {
+            next() {
+                if (index < data.length) {
+                    return {
+                        value: data[index++]
+                    };
+                } else {
+                    return {done: true};
+                }
+            }
+        };
+    }
+}
+for (const x of iterableObject) {
+    console.log(x);
+}
+
+const baseballTeam = {
+    name: "Dodgers",
+    League: "National",
+    Skipper: "Dave Roberts",
+    playBall:  ()=> {
+        return "playBall";
+    }
+}
+
+console.log(Reflect.ownKeys(iterableObject));
+console.log(Reflect.ownKeys(baseballTeam));
+console.log(baseballTeam.playBall());
